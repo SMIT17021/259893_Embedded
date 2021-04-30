f124fdbc71268b6c6b916ffb0aa7dbc3df72d515
SMIT17021 <81159296+SMIT17021@users.noreply.github.com>
2021-05-01 01:05:50 +0530

    Create Acitivity2.h

---
+++ inc/Acitivity2.h
@@ -0,0 +1,19 @@
+/**
+ * @file Activity2.h
+ * @author Smit Desai (smit082000@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+ 
+#ifndef _Activity2_h_
+#define _Activity2_h_
+
+void InitADC();
+int ReadADC(int);
+int Activity2(void);
+
+#endif
