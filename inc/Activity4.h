3f07122c92ebaacf40a5a5d8941eff5d07ec1696
SMIT17021 <81159296+SMIT17021@users.noreply.github.com>
2021-05-01 01:09:23 +0530

    Create Activity4.h

---
+++ inc/Activity4.h
@@ -0,0 +1,18 @@
+/**
+ * @file Activity4.h
+ * @author Smit Desai (smit082000@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef _Activity4_h_
+#define _Activity4_h_
+
+void USARTInit(int);
+
+void Activity4(int);
+
+#endif 
