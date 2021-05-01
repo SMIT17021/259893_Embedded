1359a8b8ed32457bc3f1302e775a5060a9442971
SMIT17021 <81159296+SMIT17021@users.noreply.github.com>
2021-05-01 10:15:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include<avr/io.h>
-#include"./inc/Activity1.h"
-#include"./inc/Activity2.h"
-#include"./inc/Activity3.h"
-#include"./inc/Activity4.h"
+#include"Activity1.h"
+#include"Activity2.h"
+#include"Activity3.h"
+#include"Activity4.h"
 
 
 int main()
